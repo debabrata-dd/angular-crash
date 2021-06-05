9ee95cf85d36cd896ad9d38381c4b064aaf74b4e
Debabrata Das <debabrata.dd@gmail.com>
2021-06-05 17:16:49 +0530

    Updated Observable and subscribe to make asyc call

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Task } from '../Task';
 import { TASKS } from '../mock-tasks';
 
@@ -8,7 +9,7 @@ import { TASKS } from '../mock-tasks';
 export class TaskService {
   constructor() {}
 
-  getTasks(): Task[] {
-    return TASKS;
+  getTasks(): Observable<Task[]> {
+    return of(TASKS);
   }
 }
