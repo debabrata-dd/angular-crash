41eed4a33c8f9e4fe4fbee33160e0e6f7b3d5591
Debabrata Das <debabrata.dd@gmail.com>
2021-06-07 22:36:24 +0530

    Added toggle to show/hide Add Task form

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 import { Task } from '../../Task';
 
 @Component({
@@ -10,17 +12,22 @@ export class AddTaskComponent implements OnInit {
   text: string = '';
   day: string = '';
   reminder: boolean = false;
+  showAddTask: boolean = false;
+  subscription: Subscription = new Subscription();
 
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
 
-  constructor() {}
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {}
 
   onSubmit() {
     if (!this.text) {
       alert('Please enter Task');
-
       return;
     }
 
